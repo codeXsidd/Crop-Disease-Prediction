35f2aa8a8cb4596fcc57e0c4467a51dbc9f1b2f3
Siddharth S <179855721+codeXsidd@users.noreply.github.com>
2026-02-26 21:23:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+
 app = FastAPI()
 
-# Allow frontend access
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,12 +15,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load model once
-model = tf.keras.models.load_model("crop_disease_model.keras")
+# Load model
+model = tf.keras.models.load_model("model.keras")
+
 
 @app.get("/")
 def home():
-    return {"message": "Crop Disease API Running"}
+    return {"message": "Crop Disease API Running Successfully"}
+
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
@@ -31,26 +33,7 @@ async def predict(file: UploadFile = File(...)):
     img_array = np.array(image) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    @app.post("/predict/")
-async def predict(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents)).convert("RGB")
-    image = image.resize((224, 224))
-
-    img_array = np.array(image) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-
-    env_array = np.array([[30, 60, 40]]) 
-
-    prediction = model.predict([img_array, env_array])
-
-    predicted_class = int(np.argmax(prediction))
-    confidence = float(np.max(prediction))
-
-    return {
-        "predicted_class": predicted_class,
-        "confidence": confidence
-    }
+    prediction = model.predict(img_array)
     predicted_class = int(np.argmax(prediction))
     confidence = float(np.max(prediction))
 
